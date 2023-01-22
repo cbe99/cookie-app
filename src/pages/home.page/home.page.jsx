323d1e33b8ee41007bff202d51698ab00c76490f
christeena-borah <christinaborah50@gmail.com>
2023-01-22 14:40:06 +0530

    add home page

---
+++ src/pages/home.page/home.page.jsx
@@ -0,0 +1,12 @@
+import {Box} from '@mui/material';
+import WelcomeCard from '../../components/app.welcome.card';
+
+const HomePage = () => {
+	return (
+		<Box>
+			<WelcomeCard />
+		</Box>
+	);
+};
+
+export default HomePage;
